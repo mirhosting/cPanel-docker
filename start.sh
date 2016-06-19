1512ca94f89adc30319ec29f5d6a9603d77a1fbd
mirhosting <dev@mirhosting.com>
2016-06-19 10:41:11 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ iptables --flush
 /scripts/restartsrv_cpsrvd
 systemctl restart mysql
 
-rm -f /root/.my.cnf;
 echo "[client]" > /root/.my.cnf;
 echo "password=" >> /root/.my.cnf;
 echo "user=root" >> /root/.my.cnf;
@@ -17,14 +16,13 @@ use mysql;
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$mysql_password');
 EOF
 
-rm -f /root/.my.cnf
-echo "[client]" > /root/.my.cnf
-echo "password=$mysql_password" >> /root/.my.cnf
-echo "user=root" >> /root/.my.cnf
+echo "[client]" > /root/.my.cnf;
+echo "password=$mysql_password" >> /root/.my.cnf;
+echo "user=root" >> /root/.my.cnf;
 
-cp /root/.my.cnf /root/.my.cnf.back
+cp /root/.my.cnf /root/.my.cnf.back;
 
-new_ip=$(ifconfig | grep 'inet'| grep -v '127.0.0.1'| cut -d: -f2 | awk '{ print $2}' |grep -v "10." |grep -v '^\s*$')
-echo $new_ip > /var/cpanel/cpnat
+new_ip=$(ifconfig | grep 'inet'| grep -v '127.0.0.1'| cut -d: -f2 | awk '{ print $2}' |grep -v "10." |grep -v '^\s*$');
+echo $new_ip > /var/cpanel/cpnat;
 
 /bin/bash
