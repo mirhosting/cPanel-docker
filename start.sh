9ec845998b8607a8040a9658020aba0a00863cbb
mirhosting <dev@mirhosting.com>
2016-06-19 10:30:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,27 @@ export LC_ALL="en_US.utf8"
 iptables --flush
 /scripts/restartsrv_cpsrvd
 systemctl restart mysql
+
+rm -f /root/.my.cnf;
+echo "[client]" > /root/.my.cnf;
+echo "password=" >> /root/.my.cnf;
+echo "user=root" >> /root/.my.cnf;
+
+mysql_password=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c8; echo "");
+
+mysql << EOF
+use mysql;
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$mysql_password');
+EOF
+
+rm -f /root/.my.cnf
+echo "[client]" > /root/.my.cnf
+echo "password=$mysql_password" >> /root/.my.cnf
+echo "user=root" >> /root/.my.cnf
+
+cp /root/.my.cnf /root/.my.cnf.back
+
+new_ip=$(ifconfig | grep 'inet'| grep -v '127.0.0.1'| cut -d: -f2 | awk '{ print $2}' |grep -v "10.")
+echo $new_ip > /var/cpanel/cpnat
+
 /bin/bash
